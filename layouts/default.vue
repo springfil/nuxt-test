ef09a18b38adde1006a69a3c395d622e13584be1
springfil <st.kenzhaev@gmail.com>
2023-11-28 15:26:14 +0300

    add module tailwind and create layout

---
+++ layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-4 flex justify-between">
+                <NuxtLink to="/" class="font-bold">Rick and Morty</NuxtLink>
+                <ul class="flex gap-4">
+                    <li><NuxtLink to="/">Home</NuxtLink></li>
+                    <li>
+                        <NuxtLink to="https://github.com/springfil/nuxt-test" 
+                        target="_blank"
+                        class="btn">
+                        Github
+                        </NuxtLink>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+        <div class="container mx-auto p-4">
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.router-link-exact-active {
+    color: #12b488;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
-  <div>
-
-  </div>
+    <div>
+        <h1 class="font-bold">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quaerat,
+            quas consectetur. Eos blanditiis voluptatibus, maxime aut itaque
+            consectetur voluptatum corporis reiciendis nihil inventore debitis
+            iure facere fugit. Porro, blanditiis optio!
+        </h1>
+    </div>
 </template>
 
-<script setup lang="ts">
+<script setup lang="ts"></script>
 
-</script>
-
-<style scoped>
-
-</style>
+<style scoped></style>
