c80ca2fb7a45a81b3c037c0cc791a0b5defb63a9
springfil <st.kenzhaev@gmail.com>
2023-11-29 04:06:39 +0300

    create page with character's info

--- pages/character/[id].vue
+++ pages/character/[id].vue
@@ -1,13 +1,27 @@
 <template>
-  <div>
-    
-  </div>
+    <div class="flex justify-around items-center shadow-2xl bg-white my-9 ">
+        <div class="p-7">
+            <img :src="`${currentCharacter.image}`" class="mx-auto my-7 rounded-full transform scale-100 transition duration-300 ease-in-out hover:scale-105" />
+        </div>
+        <div class="p-7">
+            <ul class="text-2xl my-15 ">
+              <li>Name: {{ currentCharacter.name }}</li>
+              <li>Gender: {{ currentCharacter.gender }}</li>
+              <li>Race: {{ currentCharacter.species }}</li>
+              <li>Location: {{ currentCharacter.location.name }}</li>
+            </ul>
+        </div>
+    </div>   
 </template>
 
-<script setup lang="ts">
+<script setup>
+import { useCharacterStore } from "@/store/character";
 
+const characterStore = useCharacterStore();
+const { currentCharacter } = storeToRefs(characterStore);
+
+const route = useRoute();
+characterStore.getCurrentCharacter(route.params.id);
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
